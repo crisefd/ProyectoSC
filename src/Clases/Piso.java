bede036ac3e953d3cc769efa41260182e5a5e5fc
fabianact <fabianact@s4pc17.eisc.univalle.edu.co>
2014-12-01 16:45:08 -0500

    Piso actualizado

--- src/Clases/Piso.java
+++ src/Clases/Piso.java
@@ -5,12 +5,43 @@
  */
 package Clases;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author crisefd
  */
 public class Piso {
     
+   ArrayList <Usuario>colaEspera;
+   ArrayList <Integer> tamanosColaEspera;
+   final int  MAXCOLA=10;
    
+   public Piso(){
+       colaEspera  = new ArrayList();
+       tamanosColaEspera = new ArrayList(); 
+   }
+    public void agregarUsuarioCola(Usuario u){
+        if(colaEspera.size()<=MAXCOLA){
+        colaEspera.add(u);
+        tamanosColaEspera.add(colaEspera.size());    
+        }
+        
+    }
+    public Usuario siguenteUsuarioCola(){
+        Usuario s;
+        s=colaEspera.remove(0);
+        tamanosColaEspera.add(colaEspera.size());
+        return s;
+    }
+    
+    public int calcularPromedioCola(){
+        int total=0;
+        
+        for(int n: tamanosColaEspera){
+           total+= n;
+        }
+        return (int) total/tamanosColaEspera.size();
+    }
     
 }
