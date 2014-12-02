a977051aded4bd802497936f4070d65eb11d8120
crisefd <ophiuco93@hotmail.com>
2014-12-01 20:53:36 -0500

    Distribucion Exp y generacion de pisos en simulador

--- src/Clases/Simulador.java
+++ src/Clases/Simulador.java
@@ -11,10 +11,38 @@ package Clases;
  */
 public class Simulador {
     
-    volatile int reloj;
-    int tiempoMax;
-    static public void recibirSolicitud(int pisoDeseado){
-        pisoDeseado=0;
+    private int reloj;
+    private final int TIEMPOMAX = 500;
+
+    
+    private int distribucionExponencial(double lambda, int x){
+        return (int)Math.round(lambda* Math.exp(lambda*-1* x));
+        
+    }
+    private int generarNumPiso(){ 
+        int n = (int)Math.round(Math.random()*8);
+        
+        if (n <= 3){
+            return 1;
+        }
+        if (n == 4){
+            return 2;
+        }
+        if (n == 5){
+            return 3;
+        }
+        if (n == 6){
+            return 4;
+        }
+        if (n == 7){
+            return 5;
+        }
+        if (n == 8){
+            return 6;
+        }
+        return 0;
+        
+
     }
     public static void main(String[]args){
         
