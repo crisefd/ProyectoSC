c80927891b13d59479b38ae9ca28059719b44fd6
crisefd <crisefd@s3pc11.eisc.univalle.edu.co>
2014-12-03 17:21:22 -0500

    Actualizacion ascensor

--- src/Clases/Ascensor.java
+++ src/Clases/Ascensor.java
@@ -6,19 +6,26 @@
 package Clases;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Ascensor {
 
-    private int pisoActual, capacidadMax, montados;
+    private int pisoActual, capacidadMax = 6, montados;
     private int direccion;
-    ArrayList<Integer> listaMontados= new ArrayList();
+    ArrayList<Integer> listaMontados = new ArrayList();
+    public static int tDesplazamiento = 30, tArranque = 5;
+    private boolean banderasSub[] = new boolean[6];
+    private boolean banderasBaj[] = new boolean[6];
+    //private ArrayList<Integer> listaParadas = new ArrayList<>();
 
     public Ascensor(int pisoactual) {
         this.pisoActual = pisoactual;
     }
-    
 
-    
+//    public void agregarParada(int numPiso){
+//        buscarInsertar(numPiso, 0, listaParadas.size() - 1);
+//    }
     public int getCapacidadMax() {
         return capacidadMax;
     }
@@ -43,31 +50,168 @@ public class Ascensor {
         this.direccion = direccion;
     }
 
-    public void recogerUsuarios(int usuarios){
-        montados+=usuarios;
+    public void recogerUsuarios(int usuarios) {
+        montados += usuarios;
         listaMontados.add(montados);
-    
+
     }
-    
-    public void bajarUsuarios(int usuarios){
-      montados-=usuarios;
-      listaMontados.add(montados);
+
+    public void bajarUsuarios(int usuarios) {
+        montados -= usuarios;
+        listaMontados.add(montados);
     }
-    
-    public void direccionarse(int direccion){
-        this.direccion=direccion;
+
+    public void direccionarse(int direccion) {
+        this.direccion = direccion;
     }
-    
-    public void cambiarPiso(){
-        pisoActual+=direccion;
+
+    public void cambiarPiso() {
+        pisoActual += direccion;
     }
-    
-    public int calcularPromedioMontados(){
-        int total=0;
-        
-        for(int n: listaMontados){
-           total+= n;
+
+    public int calcularPromedioMontados() {
+        int total = 0;
+
+        for (int n : listaMontados) {
+            total += n;
         }
-        return (int) total/listaMontados.size();
+        return (int) total / listaMontados.size();
+    }
+
+//    private int buscarInsertar(int numPiso, int i, int f){
+//        int tam = f - i + 1;
+//        int m = tam/2 - 1 + i;
+//        if (tam == 1){
+//            if (numPiso < listaParadas.get(i)){
+//                listaParadas.add(i, numPiso);
+//            }else{
+//                listaParadas.add(numPiso);
+//            }
+//            return 0;
+//        }else{
+//            
+//            if(numPiso < listaParadas.get(m)){
+//                return buscarInsertar(numPiso, i, m);
+//            }else{
+//                return buscarInsertar(numPiso, m + 1, f);
+//            }
+//            
+//        }
+    // }
+    private class ListaParadas {
+
+        private Integer arreglo[] = {7, 7, 7, 7, 7, 7, -1, 0, 0, 0, 0, 0, 0};
+
+        public ListaParadas() {
+            //arreglo[6] = -1;
+        }
+
+        private void agregarArr(int parada, int dire) {
+
+            if (dire == 1) {
+                for (int i = 0; i < 6; i++) {
+                    if (arreglo[i] == 7 && !banderasSub[i]) {
+                        arreglo[i] = parada;
+                        banderasSub[i] = true;
+                        break;
+                    }
+                }
+            } else {
+                for (int i = 6; i < 13; i++) {
+                    if (arreglo[i] == 0 && !banderasBaj[i]) {
+                        arreglo[i] = parada;
+                        banderasBaj[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void agregarParada(int pisoActualUsuario, int pisoDeseadoUsuario) {
+
+            int dirUsuario;
+            if (pisoActualUsuario - pisoDeseadoUsuario < 0) {
+                dirUsuario = 1;//subida
+            } else {
+                dirUsuario = -1;//bajada
+            }
+
+            if (dirUsuario == direccion && direccion == 1 && pisoDeseadoUsuario > pisoActual) {
+                Integer[] a = new Integer[6];
+                agregarArr(pisoActualUsuario, 1);
+                agregarArr(pisoDeseadoUsuario, 1);
+                System.arraycopy(arreglo, 0, a, 0, 6);
+                Arrays.sort(a);
+                System.arraycopy(a, 0, arreglo, 0, 6);
+            } else {
+                if (dirUsuario == direccion && direccion == 1 && pisoDeseadoUsuario < pisoActual) {
+                    Integer[] a = new Integer[6];
+                    Integer[] b = new Integer[6];
+                    agregarArr(pisoActualUsuario, -1);
+                    agregarArr(pisoDeseadoUsuario, 1);
+                    System.arraycopy(arreglo, 7, a, 7, 6);
+                    Arrays.sort(a);
+                    System.arraycopy(a, 7, arreglo, 7, 6);
+
+                    System.arraycopy(arreglo, 0, a, 0, 6);
+                    Arrays.sort(b);
+                    System.arraycopy(a, 0, arreglo, 0, 6);
+
+
+                } else {
+                    if (dirUsuario == direccion && direccion == -1 && pisoDeseadoUsuario < pisoActual) {
+                        Integer[] a = new Integer[6];
+                        agregarArr(pisoActualUsuario, -1);
+                        agregarArr(pisoDeseadoUsuario, -1);
+
+                        System.arraycopy(arreglo, 7, a, 7, 6);
+                        Arrays.sort(a);
+                        System.arraycopy(a, 7, arreglo, 7, 6);
+
+
+                    } else {
+
+                        if (dirUsuario == direccion && direccion == -1 && pisoDeseadoUsuario > pisoActual) {
+                            Integer[] a = new Integer[6];
+                            Integer[] b = new Integer[6];
+                            agregarArr(pisoActualUsuario, 1);
+                            agregarArr(pisoDeseadoUsuario, -1);
+                            System.arraycopy(arreglo, 7, a, 7, 6);
+                            Arrays.sort(a);
+                            System.arraycopy(a, 7, arreglo, 7, 6);
+
+                            System.arraycopy(arreglo, 0, a, 0, 6);
+                            Arrays.sort(b);
+                            System.arraycopy(a, 0, arreglo, 0, 6);
+
+
+                        } else {
+                            if (dirUsuario == 1 && direccion == -1) {
+                                Integer[] a = new Integer[6];
+                                agregarArr(pisoActualUsuario, 1);
+                                agregarArr(pisoDeseadoUsuario, 1);
+                                System.arraycopy(arreglo, 0, a, 0, 6);
+                                Arrays.sort(a);
+                                System.arraycopy(a, 0, arreglo, 0, 6);
+                            } else {
+                                if (dirUsuario == -1 && direccion == 1) {
+                                    Integer[] a = new Integer[6];
+                                    agregarArr(pisoActualUsuario, -1);
+                                    agregarArr(pisoDeseadoUsuario, -1);
+
+                                    System.arraycopy(arreglo, 7, a, 7, 6);
+                                    Arrays.sort(a);
+                                    System.arraycopy(a, 7, arreglo, 7, 6);
+
+
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
     }
 }
