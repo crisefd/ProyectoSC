ea53726bf59f397edee86a6c831dd71cc91ff7b4
crisefd <ophiuco93@hotmail.com>
2014-12-03 07:42:03 -0500

    actualizacion Main

--- src/Clases/Simulador.java
+++ src/Clases/Simulador.java
@@ -11,7 +11,7 @@ package Clases;
  */
 public class Simulador {
     
-    private int reloj, numLlegadas = 0;
+    private int reloj = 0, numLlegadas = 0;
     private final int TIEMPOMAX = 500;
     private Ascensor ascensor;
     private Piso piso1, piso2, piso3, piso4, piso5, piso6;
@@ -70,13 +70,36 @@ public class Simulador {
     
     private void generarLlegadas(){
         int numPiso = 1;
-        
-        while(numPiso <= 6){
+        int veces = 25;
+        int v=0;
+        while(v <= veces){
             int t;
-            
+            numPiso = (int)Math.round(Math.random()*5) + 1;
             if(numPiso == 1){
                 t = distribucionExponencial(0.0025, numLlegadas);
+                int p = generarNumPiso();
+                piso1.agregarUsuarioCola(new Usuario(1, p));
             }else{
+                if (numPiso == 2){
+                    int p = generarNumPiso();
+                    piso2.agregarUsuarioCola(new Usuario(2, p));
+                }
+                if (numPiso == 3){
+                    int p = generarNumPiso();
+                    piso3.agregarUsuarioCola(new Usuario(3, p));
+                }
+                if (numPiso == 4){
+                    int p = generarNumPiso();
+                    piso4.agregarUsuarioCola(new Usuario(4, p));            
+                }
+                if (numPiso == 5){
+                    int p = generarNumPiso();
+                    piso4.agregarUsuarioCola(new Usuario(5, p));    
+                }
+                if (numPiso == 6){
+                    int p = generarNumPiso();
+                    piso4.agregarUsuarioCola(new Usuario(6, p));    
+                }
                 t = distribucionExponencial(0.002, numLlegadas);
             }
             
@@ -84,6 +107,10 @@ public class Simulador {
             numLlegadas++;
             numPiso++;
         }
+    }
+    
+    private void generarViaje(){
+        
     }
     public static void main(String[]args){
         
